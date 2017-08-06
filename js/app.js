9c93e6a0d1bec73515efec82ca8c9b4f992ec6b3
Mathieu PATUREL <australie.p@gmail.com>
2017-08-06 16:34:56 +1000

    :sparkles: highlight code blocks

--- js/app.js
+++ js/app.js
@@ -11,8 +11,24 @@ function getLocationObject() {
 
 const markdownToHTML = (function () {
     const converter = new showdown.Converter()
+    converter.setOption('simplifiedAutoLink', true)
+    converter.setOption('excludeTrailingPunctuationFromURLs', true)
+    converter.setOption('tables', true)
+    converter.setOption('strikethrough', true)
+    converter.setOption('ghCodeBlocks', true)
+    converter.setOption('ghMentions', true)
+    converter.setOption('ghMentionsLink', true)
+    converter.setOption('openLinksInNewWindow', true)
+    converter.setOption('tasklists', true)
+    converter.setOption('ghCodeBlocks', true)
+
     return function (markdown) {
-        return converter.makeHtml(markdown)
+        const nodes = new DOMParser().parseFromString(converter.makeHtml(markdown), 'text/html')
+        const codes = nodes.querySelectorAll('pre code')
+        for (var i = codes.length - 1; i >= 0; i--) {
+            hljs.highlightBlock(codes[i])
+        }
+        return nodes.body.innerHTML
     }
 })()
 
@@ -68,3 +84,4 @@ xhr.addEventListener('readystatechange', function() {
         EM.fire('tips-received', JSON.parse(xhr.responseText))
     }
 }, false);
+
