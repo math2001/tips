84214ff5b456a655a408e6dd02d556810f8a0e1c
Mathieu PATUREL <australie.p@gmail.com>
2017-08-08 12:12:33 +1000

    :sparkles: adding a tag is working

--- js/add-tip.js
+++ js/add-tip.js
@@ -3,26 +3,43 @@
 class AddTip {
 
     static init() {
-        this.bindDOM()
-        this.bindEvent()
-    }
-
-    static bindDOM() {
         this.elements = {
             title: document.querySelector('#form-tip-title'),
             slug: document.querySelector('#form-tip-slug'),
-            tagsList: document.querySelector('#form-tip-tags-ist'),
+            tagsList: document.querySelector('#form-tip-tags-list'),
             tag: document.querySelector('#form-tip-tag'),
             content: document.querySelector('#form-tip-content'),
             getJson: document.querySelector('#form-get-json'),
             preview: document.querySelector('#form-preview'),
         }
+
+        this.templates = {
+            tag: document.querySelector('#tag-template').innerHTML
+        }
+
+        this.bindDOM()
+        this.bindEvent()
     }
 
-    static bindEvent() {
+    static bindDOM() {
         this.elements.title.addEventListener('input', e => {
             this.elements.slug.value = this.slugify(e.target.value)
         })
+
+        this.elements.tag.addEventListener('keydown', e => {
+            if (e.which === 13 && e.target.value != '') { // Enter
+                this.addTag(e.target.value)
+                e.target.value = ''
+            }
+        })
+    }
+
+    static bindEvent() {
+        
+    }
+
+    static addTag(tag) {
+        this.elements.tagsList.innerHTML += Mustache.render(this.templates.tag, {tag})
     }
 
     static slugify(text) {
