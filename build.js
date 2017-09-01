6dc34e8653fb7200e693069e5f20b153ef4eefe9
Mathieu PATUREL <australie.p@gmail.com>
2017-09-01 18:21:40 +1000

    :fire: remove useless files

--- build.js
+++ build.js
@@ -1,25 +1,49 @@
-const fs = require('fs')
-const showdown = require('showdown')
+const startTime = Date.now()
+const DEV = true
+if (DEV) 
+console['log']('-'.repeat(100))
+
+const {readdirSync, readFile} = require('fs')
+const {Converter: MarkdownConverter} = require('showdown')
 const hljs = require('highlight.js')
-const {JSDOM} = require('jsdom')
+const DOMParser = new (require('dom-parser'))()
 
 const HTMLParser = function (html) {
-    return new JSDOM(html).window.document
+    return DOMParser.parseFromString(html)
 }
 
 const markdownToHTML = (function () {
-    const converter = new showdown.Converter()
-    converter.setOption('simplifiedAutoLink', true)
-    converter.setOption('excludeTrailingPunctuationFromURLs', true)
-    converter.setOption('tables', true)
-    converter.setOption('strikethrough', true)
-    converter.setOption('ghMentions', true)
-    converter.setOption('ghMentionsLink', true)
-    converter.setOption('ghCodeBlocks', true)
-    converter.setOption('openLinksInNewWindow', true)
-    converter.setOption('tasklists', true)
+    const converter = new MarkdownConverter({
+        simplifiedAutoLink: true,
+        excludeTrailingPunctuationFromURLs: true,
+        tables: true,
+        strikethrough: true,
+        ghMentions: true,
+        ghMentionsLink: true,
+        ghCodeBlocks: true,
+        openLinksInNewWindow: true,
+        tasklists: true
+    })
 
     return function (markdown) {
+        let html = converter.makeHtml(markdown)
+        return html
+        const dom = HTMLParser(html)
+        let className, language, highlightedPre
+        for (let pre of dom.getElementsByTagName('pre')) {
+            className = pre.firstChild.getAttribute('class')
+            if (className === null) continue
+            language = className.split(' ').filter(str => str.startsWith('language-'))[0]
+            if (language !== undefined) language = language.slice(9)
+            index = html.indexOf(pre.outerHTML)
+            if (index === -1) {
+                throw new Error('Cannot find pre block')
+            }
+            highlightedPre = hljs.highlight(language, pre.firstChild.innerHTML).value
+            html = stringIndexReplace(html, index, index + pre.outerHTML.length, highlightedPre)
+        }
+        return html
+
         const nodes = HTMLParser(converter.makeHtml(markdown), 'text/html')
         const codes = Array.from(nodes.getElementsByTagName('pre'))
             .map(pre => pre.getElementsByTagName('code')[0])
@@ -32,6 +56,14 @@ const markdownToHTML = (function () {
     }
 })()
 
+const tips = {}
+const regexes = {
+    yamlFrontMatter: /^---\n[^]*^---\n/m,
+    lineendings: /(\r|\n|\r\n|\n\r)+/g
+}
+
+// functions (tools)
+
 function getKeyValue(string) {
     let [key, ...values] = string.split(':')
     return [key, values.join(':').trim()]
@@ -41,94 +73,123 @@ function monthToInt(month) {
     return new Date(Date.parse(month + "1, 2000")).getMonth()
 }
 
-function templatr(string, obj) {
-    Object.keys(obj).some(key => {
-        while (string.indexOf(`\${${key}}`) !== -1) {
-            string = string.replace(`\${${key}}`, obj[key])
-        }    
-    })
-    return string
-}
-
-const tipsToObject = (function () {
-
-    function parseTip(tip) {
-        tip = tip.trim()
-        let frontMatter = {},
-            isInFrontMatter = tip.startsWith('---'),
-            markdown = ''
-        tip.split('\n').slice(1).some(line => {
-            if (line === '---' && isInFrontMatter) {
-                return isInFrontMatter = false
-            }
-            if (isInFrontMatter) {
-                let [key, value] = getKeyValue(line)
-                if (key === 'tags') {
-                    value = value.split(',').map(tag => tag.trim())
-                }
-                if (key === 'date') {
-                    const matches = value.match(/(\w+) (\d+) (\w+) (\d+) @ (\d+):(\d+)/)
-                    value = new Date(parseInt(matches[4]),
-                                         monthToInt(matches[3]),
-                                         parseInt(matches[2]),
-                                         parseInt(matches[5]),
-                                         parseInt(matches[6])
-                                        ).getTime()
-                }
-                frontMatter[key] = value
-            } else {
-                markdown += line + '\n'
-            }
-        })
-        return Object.assign({}, frontMatter, {content: markdownToHTML(markdown.trim())})
-    }
-
-    return function tipsToObject() {
-        const tips = fs.readdirSync('./tips')
-            .filter(file => file.endsWith('.md'))
-
-        let callbackCount = tips.length
-        return new Promise(resolve => {
-            tips.some((file, index) => fs.readFile(`./tips/${file}`, 'utf8', (err, content) => {
-                if (err) throw err
-                tips[index] = parseTip(content)
-                callbackCount -= 1
-                if (callbackCount === 0) resolve(tips.sort((tipa, tipb) => tipb.date - tipa.date))
-            }))
-        })
-
-    }
-})()
-
-function getTemplate() {
+function loadFile(file) {
     return new Promise((resolve, reject) => {
-        fs.readFile('./index.template.html', 'utf8', (err, content) => {
-            if (err) {
-                reject(err)
-                throw err
-            }
+        readFile(`${file}`, 'utf8', (err, content) => {
+            if (err) throw err
             resolve(content)
         })
     })
 }
 
-if (require.main === module) {
-    Promise.all([tipsToObject(), getTemplate()]).then(args => {
-        const [tips, template] = args
-        return template.replace('JSON_TIPS', JSON.stringify(tips).replace(/\\/g, '\\\\').replace(/'/g, "\\'"))
-    }).then(html => {
-        return new Promise((resolve, reject) => {
-            fs.writeFile('./index.html', html, err => {
-                if (err) reject(err)
-                console.info("Successfully written 'index.html'!")
-            })
-        })
-    }).catch(err => {
-        console.error("Error while writing the file 'index.html'")
-        throw err
-    })
-} else {
-    module.exports = {
-        templatr: templatr
-    }
+function stringIndexReplace(originalString, indexStart, indexEnd, string) {
+    return originalString.slice(0, indexStart) + string + originalString.slice(indexEnd)
 }
+
+// functions
+
+function loadTip(file) {
+    // asyncally parse tip
+    return loadFile(`./tips/${file}`).then(content => {
+        return new Promise((resolve, reject) => {
+            setTimeout(function () {
+                resolve(parseTip(content, file))
+            }, 0)
+        })
+    })
+}
+
+function loadTips() {
+    const files = readdirSync('./tips').filter(file => file.endsWith('.md'))
+    console.info('Got dirs...')
+    const promises = []
+    for (let file of files) {
+        promises.push(loadTip(file))
+    }
+    return Promise.all(promises)
+}
+
+function parseTip(content, filename) {
+    const parseFrontMatter = frontMatter => {
+        const obj = {}
+        frontMatter = frontMatter.trim().split('\n')
+        for (let line of frontMatter) {
+            let [key, value] = getKeyValue(line)
+            if (key === 'tags') {
+                value = value.split(',').map(tag => tag.trim())
+            }
+            if (key === 'date') {
+                const matches = value.match(/(\w+) (\d+) (\w+) (\d+) @ (\d+):(\d+)/)
+                value = new Date(parseInt(matches[4]), monthToInt(matches[3]), parseInt(matches[2]),
+                                 parseInt(matches[5]), parseInt(matches[6])).getTime()
+            }
+            obj[key] = value
+        }
+        return obj
+    }
+
+    const parse = content => {
+        content = content.replace(regexes.lineendings, '\n')
+        const matches = regexes.yamlFrontMatter.exec(content)
+        let frontMatter = {}
+        if (matches !== null) {
+            content = content.slice(matches[0].length).trim()
+            frontMatter = parseFrontMatter(matches[0].slice(4, -4))
+        } else {
+            console.warn(`The file ${filename} doesn't have any yaml front matter`)
+        }
+        content = markdownToHTML(content)
+        return Object.assign({}, frontMatter, { content: content })
+    }
+
+    return Object.assign({
+        filename: filename,
+    }, parse(content))
+}
+
+function fileLinks(tips) {
+    let dom, to, innerHTML, actualLink, tip
+    for (let filename in tips) {
+        dom = HTMLParser(tips[filename].content)
+        for (let link of dom.getElementsByTagName('tiplink')) {
+            actualLink = '<a href="'
+            to = link.getAttribute('to')
+            if (to === null) {
+                console.error(`<tiplink> hasn't got a 'to' attribute in ${filename}`)
+                continue
+            }
+            tip = tips[to]
+            if (tip === undefined) {
+                console.error(`<tiplink>'s 'to' attribute points to a ` +
+                                `non-existing file ('${to}') in '${filename}'`)
+                continue
+            }
+            actualLink += '#' + tip.slug + '">'
+            if (link.innerHTML !== '') actualLink += link.innerHTML
+            else actualLink += tip.title
+            actualLink += '</a>'
+            index = tips[filename].content.indexOf(link.outerHTML)
+            if (index === -1) throw new Error('Cannot find original link to replace it with')
+            tips[filename].content = stringIndexReplace(tips[filename].content, index,
+                                                        index + link.outerHTML.length, actualLink)
+        }
+    }
+    return tips
+}
+
+function main() {
+    loadTips().then(tips => {
+        const formattedTips = {}
+        for (let tip of tips) {
+            formattedTips[tip.filename] = tip
+            delete formattedTips[tip.filename].filename
+        }
+        return fileLinks(formattedTips)
+    }).then(tips => {
+        console.info(`Done parsing ${Object.keys(tips).length} tips in ${Date.now() - startTime }ms.`)
+    }).catch(err => {
+        console.error(err.stack)
+    })
+}
+
+main()
